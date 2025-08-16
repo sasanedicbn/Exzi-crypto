ad00a8bec655499c1dfb6f5f3dc22106b81f8189
Sasa Nedic <svetasmak0@gmail.com>
2025-08-16 21:22:09 +0200

    Create Button

--- src/components/structure/Header.tsx
+++ src/components/structure/Header.tsx
@@ -1,10 +1,17 @@
-const Header = () => {
-  return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold">Order Trading Platform</h1>
-      <Button>Connect Wallet</Button>
-    </header>
-  );
-};
+import Button from "../reusable/Button";
 
+const Header = () => {
+  const connectWallet = () => {
+    console.log("connect wallet");
+
+    return (
+      <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-2xl font-bold">Order Trading Platform</h1>
+        <Button type="primary-btn" onClick={connectWallet}>
+          Connect Wallet
+        </Button>
+      </header>
+    );
+  };
+};
 export default Header;
